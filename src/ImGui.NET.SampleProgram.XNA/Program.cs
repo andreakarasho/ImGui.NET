f5b4d3d37f5bcb74dd27f2f43f67b6afa9ff0c04
Andrea Angelini <andrea-angelini@outlook.it>
2020-11-26 14:40:00 +0100

    dll map

--- src/ImGui.NET.SampleProgram.XNA/Program.cs
+++ src/ImGui.NET.SampleProgram.XNA/Program.cs
@@ -1,9 +1,13 @@
-﻿namespace ImGuiNET.SampleProgram.XNA
+﻿using Microsoft.Xna.Framework;
+
+namespace ImGuiNET.SampleProgram.XNA
 {
     public static class Program
     {
         public static void Main(string[] args)
         {
+            CoreDllMap.Register(typeof(Color).Assembly);
+
             using (var game = new SampleGame()) game.Run();
         }
     }
