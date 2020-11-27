80a4bbfc11f17c4dc2f4d83cbe480938daad0eb7
Andrea Angelini <andrea-angelini@outlook.it>
2020-11-27 21:39:48 +0100

    log

--- src/ImGui.NET.SampleProgram.XNA/Program.cs
+++ src/ImGui.NET.SampleProgram.XNA/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Xna.Framework;
+using System;
 
 namespace ImGuiNET.SampleProgram.XNA
 {
@@ -7,7 +8,22 @@ namespace ImGuiNET.SampleProgram.XNA
         public static void Main(string[] args)
         {
             CoreDllMap.Register(typeof(Color).Assembly);
-            
+
+            FNALoggerEXT.LogError += (e) =>
+            {
+                Console.WriteLine("[ERROR]: {0}", e);
+            };
+
+            FNALoggerEXT.LogWarn += (e) =>
+            {
+                Console.WriteLine("[WARN]: {0}", e);
+            };
+
+            FNALoggerEXT.LogInfo += (e) =>
+            {
+                Console.WriteLine("[INFO]: {0}", e);
+            };
+
             //System.Environment.SetEnvironmentVariable("FNA3D_OPENGL_FORCE_ES3", "1");
             //System.Environment.SetEnvironmentVariable("FNA3D_FORCE_DRIVER", "Vulkan");
 
