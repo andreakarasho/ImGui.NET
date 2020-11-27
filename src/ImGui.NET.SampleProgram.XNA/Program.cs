c9421d26855ec18720aba39285fb47117600be80
Andrea Angelini <andrea-angelini@outlook.it>
2020-11-27 21:26:04 +0100

    no vulkan

--- src/ImGui.NET.SampleProgram.XNA/Program.cs
+++ src/ImGui.NET.SampleProgram.XNA/Program.cs
@@ -9,7 +9,7 @@ namespace ImGuiNET.SampleProgram.XNA
             CoreDllMap.Register(typeof(Color).Assembly);
             
             //System.Environment.SetEnvironmentVariable("FNA3D_OPENGL_FORCE_ES3", "1");
-            System.Environment.SetEnvironmentVariable("FNA3D_FORCE_DRIVER", "Vulkan");
+            //System.Environment.SetEnvironmentVariable("FNA3D_FORCE_DRIVER", "Vulkan");
 
             using (var game = new SampleGame()) game.Run();
         }
