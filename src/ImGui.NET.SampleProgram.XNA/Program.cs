ec869bce369c22c76429e6bc7fe501c950cd6567
Andrea Angelini <andrea-angelini@outlook.it>
2020-11-27 20:48:52 +0100

    ES3

--- src/ImGui.NET.SampleProgram.XNA/Program.cs
+++ src/ImGui.NET.SampleProgram.XNA/Program.cs
@@ -7,7 +7,8 @@ namespace ImGuiNET.SampleProgram.XNA
         public static void Main(string[] args)
         {
             CoreDllMap.Register(typeof(Color).Assembly);
-
+            
+            System.Environment.SetEnvironmentVariable("FNA3D_OPENGL_FORCE_ES3", "1");
             //System.Environment.SetEnvironmentVariable("FNA3D_FORCE_DRIVER", "Vulkan");
 
             using (var game = new SampleGame()) game.Run();
